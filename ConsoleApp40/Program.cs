3a37b7a8334558a429301929e72919beec7e379c
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-12-13 22:55:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string phrase, int deep)
+        {
+            Console.WriteLine(phrase);
+
+            if (deep > 1)
+            {
+                Echo(phrase, deep - 1);
+            }
+
+            static void Echo(string saidworld, int deep)
+            {
+                var modif = saidworld;
+                if (modif.Length > 2)
+                {
+                    modif = modif.Remove(0, 2);
+                }
+                Console.WriteLine(" " + modif);
+                if (deep > 1)
+                {
+                    Echo(modif, deep - 1);
+                }
+            }
+        }
+    }
+}
+
